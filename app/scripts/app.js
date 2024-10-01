4a37863bfd2717cf83cd6e117449ed78773012b6
Javier Galvan <javier.galvan@controlnet.local>
2024-10-01 15:29:22 +0200

    improved styles

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,7 +4,6 @@ document.onreadystatechange = function () {
   function renderApp() {
     var onInit = app.initialized();
 
-
     onInit.then(function (_client) {
       window.client = _client;
       getDocuments()
@@ -330,8 +329,20 @@ productForm.addEventListener('submit', function (event) {
               <td>${productQuantity}</td>
               <td class="product-subtotal">${productSubtotal.toFixed(2)}</td>
               <td>
-                  <button class="edit-button">Edit</button>
-                  <button class="delete-button">Delete</button>
+              <div class="flexRow">
+                  <button class="edit-button actionButton actionFlex">
+                  <svg width="15px" height="15px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M21.2799 6.40005L11.7399 15.94C10.7899 16.89 7.96987 17.33 7.33987 16.7C6.70987 16.07 7.13987 13.25 8.08987 12.3L17.6399 2.75002C17.8754 2.49308 18.1605 2.28654 18.4781 2.14284C18.7956 1.99914 19.139 1.92124 19.4875 1.9139C19.8359 1.90657 20.1823 1.96991 20.5056 2.10012C20.8289 2.23033 21.1225 2.42473 21.3686 2.67153C21.6147 2.91833 21.8083 3.21243 21.9376 3.53609C22.0669 3.85976 22.1294 4.20626 22.1211 4.55471C22.1128 4.90316 22.0339 5.24635 21.8894 5.5635C21.7448 5.88065 21.5375 6.16524 21.2799 6.40005V6.40005Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M11 4H6C4.93913 4 3.92178 4.42142 3.17163 5.17157C2.42149 5.92172 2 6.93913 2 8V18C2 19.0609 2.42149 20.0783 3.17163 20.8284C3.92178 21.5786 4.93913 22 6 22H17C19.21 22 20 20.2 20 18V13" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+                  Edit
+                  </button>
+                  <button class="delete-button actionButton actionFlex">
+                  <?xml version="1.0" encoding="utf-8"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
+<svg width="15px" height="15px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg"><path fill="#000000" d="M160 256H96a32 32 0 0 1 0-64h256V95.936a32 32 0 0 1 32-32h256a32 32 0 0 1 32 32V192h256a32 32 0 1 1 0 64h-64v672a32 32 0 0 1-32 32H192a32 32 0 0 1-32-32V256zm448-64v-64H416v64h192zM224 896h576V256H224v640zm192-128a32 32 0 0 1-32-32V416a32 32 0 0 1 64 0v320a32 32 0 0 1-32 32zm192 0a32 32 0 0 1-32-32V416a32 32 0 0 1 64 0v320a32 32 0 0 1-32 32z"/></svg>
+                  Delete
+                  </button>
+                  </div>
               </td>
           `;
 
@@ -707,8 +718,20 @@ if (index !== -1) {  // Si el producto existe en el array
               <td>${product.quantity}</td>
               <td class="product-subtotal">${product.unit_price * product.quantity}</td>
               <td>
-                  <button class="edit-button">Edit</button>
-                  <button class="delete-button">Delete</button>
+              <div class="flexRow">
+                  <button class="edit-button actionButton actionFlex">
+                  <svg width="15px" height="15px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M21.2799 6.40005L11.7399 15.94C10.7899 16.89 7.96987 17.33 7.33987 16.7C6.70987 16.07 7.13987 13.25 8.08987 12.3L17.6399 2.75002C17.8754 2.49308 18.1605 2.28654 18.4781 2.14284C18.7956 1.99914 19.139 1.92124 19.4875 1.9139C19.8359 1.90657 20.1823 1.96991 20.5056 2.10012C20.8289 2.23033 21.1225 2.42473 21.3686 2.67153C21.6147 2.91833 21.8083 3.21243 21.9376 3.53609C22.0669 3.85976 22.1294 4.20626 22.1211 4.55471C22.1128 4.90316 22.0339 5.24635 21.8894 5.5635C21.7448 5.88065 21.5375 6.16524 21.2799 6.40005V6.40005Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M11 4H6C4.93913 4 3.92178 4.42142 3.17163 5.17157C2.42149 5.92172 2 6.93913 2 8V18C2 19.0609 2.42149 20.0783 3.17163 20.8284C3.92178 21.5786 4.93913 22 6 22H17C19.21 22 20 20.2 20 18V13" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+                  Edit
+                  </button>
+                  <button class="delete-button actionButton actionFlex">
+                  <?xml version="1.0" encoding="utf-8"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
+<svg width="15px" height="15px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg"><path fill="#000000" d="M160 256H96a32 32 0 0 1 0-64h256V95.936a32 32 0 0 1 32-32h256a32 32 0 0 1 32 32V192h256a32 32 0 1 1 0 64h-64v672a32 32 0 0 1-32 32H192a32 32 0 0 1-32-32V256zm448-64v-64H416v64h192zM224 896h576V256H224v640zm192-128a32 32 0 0 1-32-32V416a32 32 0 0 1 64 0v320a32 32 0 0 1-32 32zm192 0a32 32 0 0 1-32-32V416a32 32 0 0 1 64 0v320a32 32 0 0 1-32 32z"/></svg>
+                  Delete
+                  </button>
+                  </div>
               </td>
           `;
           productTable.appendChild(row);
@@ -732,7 +755,7 @@ function limpiarQuotes() {
 
   onInit.then(function (_client) {
     window.client = _client;
-
+    
 
 
     // Obtener los datos actuales de 'Quotes'
@@ -753,8 +776,12 @@ function limpiarQuotes() {
         console.error("No se encontró la estructura de datos de Quotes.");
       }
     }).catch(function (error) {
-      console.error("Error al obtener los datos de Quotes:", error);
+      // console.error("Error al obtener los datos de Quotes:", error);
+      throw new Error("Currency ID not found. Please verify the currency list and the deal data.", error);
     });
+  })
+  .catch((err)=>{
+    console.log(err)
   });
 }
 
